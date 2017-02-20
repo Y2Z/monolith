3f499824f8eec0e8c1ea6ee91e0cbcd62d3a7f68
vflyson <vflyson@users.noreply.github.com>
2017-02-20 04:09:46 -0500

    Can't touch #this

--- modules/anchors.js
+++ modules/anchors.js
@@ -11,6 +11,10 @@ module.exports = {
             if (anchors[i].getAttribute('href')) {
                 var anchor = anchors[i]
                 var href = anchor.getAttribute('href').trim()
+
+                // Do not touch hrefs which start with a pound sign
+                if (href[0] == '#') continue
+
                 var absoluteURL = functions.resolve(absBasePath, href)
 
                 anchor.setAttribute('href', absoluteURL)
