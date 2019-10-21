90cf06285f6acc9108ef41d0da64617c9cc08a72
Emmanuel Delaborde <emmanuel.delaborde@modicagroup.com>
2019-10-21 16:15:19 +1300

    Use HashMap as cache to minimize the number of HTTP requests

--- src/tests/http.rs
+++ src/tests/http.rs
@@ -1,13 +1,23 @@
 use crate::http::retrieve_asset;
-
+use std::collections::HashMap;
 #[test]
 fn test_retrieve_asset() {
-    let (data, final_url) =
-        retrieve_asset("data:text/html;base64,...", true, "", "", true, false).unwrap();
+    let cache = &mut HashMap::new();
+    let (data, final_url) = retrieve_asset(
+        cache,
+        "data:text/html;base64,...",
+        true,
+        "",
+        "",
+        true,
+        false,
+    )
+    .unwrap();
     assert_eq!(&data, "data:text/html;base64,...");
     assert_eq!(&final_url, "data:text/html;base64,...");
 
     let (data, final_url) = retrieve_asset(
+        cache,
         "data:text/html;base64,...",
         true,
         "image/png",
