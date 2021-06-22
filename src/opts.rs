4cfd332648e26221c2d6ea18e765bc207a630a0a
Tobi823 <Tobi823@users.noreply.github.com>
2021-06-22 17:21:05 +0200

    improve description of option "--cookies"

--- src/opts.rs
+++ src/opts.rs
@@ -69,7 +69,7 @@ impl Options {
             .args_from_usage("-t, --timeout=[60] 'Adjusts network request timeout'")
             .args_from_usage("-u, --user-agent=[Firefox] 'Sets custom User-Agent string'")
             .args_from_usage("-v, --no-video 'Removes video sources'")
-            .args_from_usage("--cookies, --cookies=[UTF-8] 'Set cookies for HTTP requests. This format is being used: name1=value1;name2=value2'")
+            .args_from_usage("--cookies, --cookies=[UTF-8] 'Set cookies for HTTP requests. This format is being used: \"name1=value1;name2=value2\"'")
             .arg(
                 Arg::with_name("target")
                     .required(true)
