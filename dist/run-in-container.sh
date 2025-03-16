09a2798f67f8c99e129f7d8f72034863ded0a0c7
Sunshine <snshn@tutanota.com>
2025-03-16 16:13:06 -0100

    make it possible to use output file option from within container

--- dist/run-in-container.sh
+++ dist/run-in-container.sh
@@ -6,5 +6,15 @@ if which podman 2>&1 > /dev/null; then
 fi
 ORG_NAME=y2z
 PROG_NAME=monolith
+DOCKER_IMAGE_TAG=$ORG_NAME/$PROG_NAME
+TEMP_DIRECTORY=/tmp/monolith-$(openssl rand -hex 4)
 
-$DOCKER run --rm $ORG_NAME/$PROG_NAME "$@"
+mkdir TEMP_DIRECTORY
+
+if [ -d "$TEMP_DIRECTORY" ]; then
+    $DOCKER run --rm -v "$TEMP_DIRECTORY:/mnt" $DOCKER_IMAGE_TAG $@
+    mv $TEMP_DIRECTORY/* .
+    rmdir TEMP_DIRECTORY
+else
+    $DOCKER run --rm $ORG_NAME/$PROG_NAME "$@"
+fi
